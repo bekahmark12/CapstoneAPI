94f467669b7625eed996657801dfc472bbe3487f
yhung-mea7 <60412916+yhung-mea7@users.noreply.github.com>
2021-07-12 07:41:10 -0600

    update cors on cart api

--- react-shopping-cart/src/APIClients/CartClient.js
+++ react-shopping-cart/src/APIClients/CartClient.js
@@ -1,6 +1,22 @@
 import axios from "axios";
 class CartClient{
 
+    static postItemToCart = async(itemId, itemQty) =>{
+        try{
+            const request = await axios.post(
+                `http://localhost:8080/api/cart/${itemId}qty=${itemQty}`,
+                { headers: { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InRlc3RAZ21haWwuY29tIiwiZXhwIjoxNjI2MTI5NzMyLCJpc3MiOiJ1c2VyLXNlcnZpY2UifQ.f9Orlj-ZszMIaX9mWFa5h6ZcsbSdbbqPi-YGNlHM7VY" } }
+
+            )
+            return {succeeded: true, data: create.data};
+        } catch (err) {
+            if (err.response) {
+                return {succeeded: false, data: err.response};
+            }
+            return {succeeded: false, data: "unexpected error :("};
+        }
+    }
+
 
 }
 export default CartClient
