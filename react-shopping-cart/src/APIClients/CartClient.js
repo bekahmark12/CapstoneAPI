a8993861c3368004bd85273e07d7b2e38b61f9ac
bekahmark12 <rebekahmark12@gmail.com>
2021-07-12 09:12:12 -0600

    front end stuff

--- react-shopping-cart/src/APIClients/CartClient.js
+++ react-shopping-cart/src/APIClients/CartClient.js
@@ -1,22 +1,21 @@
 import axios from "axios";
 
 class CartClient {
-    // static async getKeys() {
-    //     return localStorage.getItem("")
-    // }
-
     static async postCart(item_id, quantity) {
         try {
             const create = await axios.post(
                 `http://localhost:8080/api/cart/${item_id}?qty=${quantity}`,
-                { headers: { "Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InRlc3RAZ21haWwuY29tIiwiZXhwIjoxNjI2MTM2NDU3LCJpc3MiOiJ1c2VyLXNlcnZpY2UifQ.XslFSleIe6OREDaxoEnE5uwODXniElLOpP46r0kttA0" } }
+                null,
+                { headers: {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InRlc3RAZ21haWwuY29tIiwiZXhwIjoxNjI2MTg3NTE1LCJpc3MiOiJ1c2VyLXNlcnZpY2UifQ.nu4kOswTftX9hL5BoE8GSJtmTteeLarRhvCbKfIrrss" } }
             );
             return {succeeded: true, data: create.data};
         } catch (err) {
             if (err.response) {
+                console.log(err.response)
                 return {succeeded: false, data: err.response};
             }
-            return {succeeded: false, data: "unexpected error :("};
+            console.log(err.response)
+            return {succeeded: false, data: "unexpected error"};
         }
     }
 }
--- react-shopping-cart/src/APIClients/CheckoutClient.js
+++ react-shopping-cart/src/APIClients/CheckoutClient.js
@@ -1,16 +1,12 @@
 import axios from "axios";
 
 class CheckoutClient {
-    // static async getKeys() {
-    //     return localStorage.getItem("")
-    // }
-
     static async postCheckout(item) {
         try {
             const create = await axios.post(
                 "http://localhost:8080/api/checkout/",
                 JSON.stringify(item),
-                { headers: { "Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InRlc3RAZ21haWwuY29tIiwiZXhwIjoxNjI2MTM2NDU3LCJpc3MiOiJ1c2VyLXNlcnZpY2UifQ.XslFSleIe6OREDaxoEnE5uwODXniElLOpP46r0kttA0" } }
+                { headers: { "Content-Type": "application/json", "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJFbWFpbCI6InRlc3RAZ21haWwuY29tIiwiZXhwIjoxNjI2MTg3NTE1LCJpc3MiOiJ1c2VyLXNlcnZpY2UifQ.nu4kOswTftX9hL5BoE8GSJtmTteeLarRhvCbKfIrrss" } }
             );
             return {succeeded: true, data: create.data};
         } catch (err) {
